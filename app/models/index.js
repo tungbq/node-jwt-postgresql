bbb3cfc3626cb8fa0f255dddd9df8993a91fe48c
Tung Bui <tungbq.info@gmail.com>
2021-10-14 22:59:00 +0700

    Add models

---
+++ app/models/index.js
@@ -0,0 +1,39 @@
+const config = require('../config/db.config.js');
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(config.DB, config.USER, config.PASSWORD, {
+	host: config.HOST,
+	dialect: config.dialect,
+	operatorsAliases: false,
+
+	pool: {
+		max: config.pool.max,
+		min: config.pool.min,
+		acquire: config.pool.acquire,
+		idle: config.pool.idle,
+	},
+});
+
+const db = {};
+
+db.Sequelize = Sequelize;
+db.sequelize = sequelize;
+
+db.user = require('../models/user');
+db.role = require('../models/role');
+
+db.role.belongsToMany(db.user, {
+	through: 'user_roles',
+	foreign_key: 'roleID',
+	otherKey: 'userID',
+});
+
+db.user.belongsToMany(db.role, {
+	through: 'user_roles',
+	foreign_key: 'userID',
+	otherKey: 'roleID',
+});
+
+db.ROLES = ['user', 'admin', 'password'];
+
+module.exports = db;
