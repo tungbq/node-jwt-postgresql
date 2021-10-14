8b8c1ebe8346e923fef402f3ff6ab3e9600a8540
Tung Bui <tungbq.info@gmail.com>
2021-10-14 23:04:16 +0700

    Add db module into index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const bodyParser = require("body-parser");
-const cors = require("cors");
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
 let corsOptions = {
-  origin: "http://localhost:8081"
+	origin: 'http://localhost:8081',
 };
 
 app.use(cors(corsOptions));
@@ -13,15 +13,41 @@ app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 //Parse requests of content-type - application/json
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Place where database comes in
+const db = require('./app/models');
+const Role = db.role;
+
+// Only force for dev environment
+db.sequelize.sync({ force: true }).then(() => {
+	console.log('Drop and sync the DB');
+	initialize();
+});
+
+// Feed DB data
+function initialize() {
+	Role.create({
+		id: 1,
+		name: 'user',
+	});
+	Role.create({
+		id: 2,
+		name: 'moderator',
+	});
+	Role.create({
+		id: 1,
+		name: 'admin',
+	});
+}
 
 // Basic route
-app.get("/", (req, res) => {
-  res.json({message: "Hi there!"})
+app.get('/', (req, res) => {
+	res.json({ message: 'Hi there!' });
 });
 
 const PORT = process.env.PORT || 1995;
 
 app.listen(PORT, () => {
-  console.log(`Server is up and running on port ${PORT}`);
+	console.log(`Server is up and running on port ${PORT}`);
 });
